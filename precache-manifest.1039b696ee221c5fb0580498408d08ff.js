d95b2d6308169fcb607aa561b57e9ceaa07c2550
MacksonWelton <mackson.welton@gmail.com>
2020-07-10 18:13:53 -0300

    Portfolio

---
+++ precache-manifest.1039b696ee221c5fb0580498408d08ff.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c7bfeeb5140263334b832c3c84c1a5f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "103c75f69aba73d4da9b",
+    "url": "/static/js/2.e79ba673.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/static/js/2.e79ba673.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e11508d738d30d4ac1a",
+    "url": "/static/js/main.3c342ded.chunk.js"
+  },
+  {
+    "revision": "ea6835d4b1bdf6a984a4",
+    "url": "/static/js/runtime-main.18f20938.js"
+  },
+  {
+    "revision": "9b37e9f7c703e8e97d1707ba1ab386d2",
+    "url": "/static/media/Astromatch.9b37e9f7.png"
+  },
+  {
+    "revision": "3982a3001affea74f15da06add010d68",
+    "url": "/static/media/Currículo.3982a300.pdf"
+  },
+  {
+    "revision": "b51b534215c70ea3393f6e27a9e8c47b",
+    "url": "/static/media/Spotenu.b51b5342.png"
+  },
+  {
+    "revision": "199b898e1693adc8e9c319dfe18b589c",
+    "url": "/static/media/foto.199b898e.jpg"
+  },
+  {
+    "revision": "fec7da6d162ffc5e35ee5bdf873db0e7",
+    "url": "/static/media/github.fec7da6d.svg"
+  },
+  {
+    "revision": "a3dc9768b526200641b18f3fc840837b",
+    "url": "/static/media/iFuture.a3dc9768.png"
+  },
+  {
+    "revision": "13ab15674b6da7aec96f9a027080f42b",
+    "url": "/static/media/instagram.13ab1567.svg"
+  },
+  {
+    "revision": "11a5ec5fa159c59de33f42020a24d1d5",
+    "url": "/static/media/linkedin.11a5ec5f.svg"
+  },
+  {
+    "revision": "c63fe684d46f411854ddd21abbca123b",
+    "url": "/static/media/notebook.c63fe684.png"
+  },
+  {
+    "revision": "a214b6500600844419cd8a949e83ecf2",
+    "url": "/static/media/twitter.a214b650.svg"
+  },
+  {
+    "revision": "519386955c4c084c80c6ee650214e36e",
+    "url": "/static/media/whatsapp.51938695.svg"
+  }
+]);
